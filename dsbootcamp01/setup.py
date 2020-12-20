9422a5cfa40e907dd11596be812bb2e27abb2d2a
kenangs01 <66200735+kenangs01@users.noreply.github.com>
2020-12-20 11:19:18 +0300

    setup file' dan once son cikis

---
+++ dsbootcamp01/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(name="dsbootcamp",
+                 version="0.0.1",
+                 license="MIT",
+                 author="Kenan Arslanbay",
+                 author_mail="kenanarslanbay@gmail.com",
+                 description="Data Science Tools Package",
+                 url="https://github.com/kenangs01/dsbootcamp",
+                 package=setuptools.find_packages(),
+                 key_words=["data science","machine learning","bootcamp"],
+                 python_requires=">=3.6")
